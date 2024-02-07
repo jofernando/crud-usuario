9211e12c171d373f29465a644bd7bb852a3a639a
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2024-02-07 13:36:28 -0300

    inicio do projeto com rest

---
+++ src/test/java/br/edu/ufape/crudusuarios/CrudUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.crudusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
